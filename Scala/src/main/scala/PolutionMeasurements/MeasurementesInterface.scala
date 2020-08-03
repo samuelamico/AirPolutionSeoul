394b56617b2886d2dc7c10c5e38500d937173ae5
samuelamico <sam.fst@gmail.com>
2020-08-03 16:12:02 -0300

    Trait update and complete

--- Scala/src/main/scala/PolutionMeasurements/MeasurementesInterface.scala
+++ Scala/src/main/scala/PolutionMeasurements/MeasurementesInterface.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.{Dataset, Row}
 
 trait MeasurementesInterface {
   def readStation(stationfile: String): Dataset[Row]
-  def readItem(itemfile: String): Dataset[Row]
+  def readItem(itemfile: String): Map[Any, levelRisk]
   def readMeasurement(measuredfile: String): Dataset[Row]
   def joinedDF(stationDF: Dataset[Row],itemDF: Dataset[Row],measureDF: Dataset[Row]): Dataset[Row]
   def convertTimezone(timezone: String): String
